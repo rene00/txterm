f05806c3ccdbb0725629b288f6bb274ce08e7616
Rene Cunningham <rene@compounddata.com>
2023-06-18 13:15:21 +1000

    store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,17 +1,12 @@
 package store
 
 import (
-	"context"
 	"database/sql"
 	"errors"
 	"fmt"
-	"math/big"
 	"os"
 	"path/filepath"
-	"strings"
-	"time"
 	"txterm/db"
-	"txterm/db/query"
 
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
@@ -23,15 +18,9 @@ const (
 	DBName = "txterm.db"
 )
 
-type AccountType struct {
-	ID   int64
-	Name string
-}
-
 type Store struct {
-	db      *sql.DB
-	dir     string
-	queries *query.Queries
+	DB  *sql.DB
+	dir string
 }
 
 func (s *Store) DBPath() string {
@@ -51,8 +40,6 @@ func (s *Store) init() error {
 		return fmt.Errorf("initDB: %w", err)
 	}
 
-	s.queries = query.New(s.db)
-
 	return nil
 }
 
@@ -61,7 +48,7 @@ func (s *Store) initDB() error {
 	if err != nil {
 		return err
 	}
-	s.db = db
+	s.DB = db
 	return nil
 }
 
@@ -86,18 +73,18 @@ func (s *Store) migrate() error {
 	return nil
 }
 
-func New() (*Store, error) {
-	store := &Store{}
+func New() (Store, error) {
+	store := Store{}
 	if store.dir == "" {
 		dir, err := defaultStorePath()
 		if err != nil {
-			return nil, err
+			return store, err
 		}
 		store.dir = dir
 	}
 
 	if err := store.init(); err != nil {
-		return nil, fmt.Errorf("init: %w", err)
+		return store, err
 	}
 
 	return store, nil
@@ -126,162 +113,4 @@ func ensureDir(dir string) error {
 		return fmt.Errorf("%q exists but is a file", dir)
 	}
 	return nil
-}
-
-func (s *Store) GetAccount(ctx context.Context, name, accountTypeName string) (Account, error) {
-	var account Account
-
-	ar, err := s.queries.GetAccount(ctx, query.GetAccountParams{
-		Name:   name,
-		Name_2: accountTypeName,
-	})
-	if err != nil {
-		return account, fmt.Errorf("failed account: %w", err)
-	}
-
-	account = Account{
-		id:          ar.ID,
-		name:        ar.Name,
-		description: ar.Description.String,
-		accountType: AccountType{
-			ID:   ar.AccountTypeID,
-			Name: ar.AccountTypeName,
-		},
-	}
-
-	return account, nil
-}
-
-func (s *Store) CreateAccount(ctx context.Context, name, description, accountTypeName string, parentAccountID int64) (Account, error) {
-	var account Account
-
-	at, err := s.queries.GetAccountType(ctx, accountTypeName)
-	switch {
-	case errors.Is(err, sql.ErrNoRows):
-		at, err = s.queries.CreateAccountType(ctx, accountTypeName)
-		if err != nil {
-			return account, fmt.Errorf("failed creating account type: %s, %w", accountTypeName, err)
-		}
-	case err != nil:
-		return account, fmt.Errorf("failed to get account type: %s, %w", accountTypeName, err)
-	}
-
-	account, err = s.GetAccount(ctx, name, accountTypeName)
-	switch {
-	case err == sql.ErrNoRows:
-		a, err := s.queries.CreateAccount(ctx, query.CreateAccountParams{
-			Name:          name,
-			Description:   sql.NullString{String: description, Valid: true},
-			AccountTypeID: at.ID,
-		})
-		if err != nil {
-			return account, fmt.Errorf("failed creating account: %s, %s, %w", name, at.Name, err)
-		}
-		account = Account{
-			id:          a.ID,
-			name:        a.Name,
-			description: a.Description.String,
-			accountType: AccountType{
-				ID:   at.ID,
-				Name: at.Name,
-			},
-		}
-	case err != nil:
-		return account, err
-	}
-
-	return account, nil
-}
-
-// SaveImport persists the import "run" to storage.
-func (s *Store) SaveImport(ctx context.Context, filename string, balanceAmount big.Rat, dateAsOf time.Time) (*Import, error) {
-	imprt, err := s.queries.CreateImport(ctx, query.CreateImportParams{
-		DateCreated:      time.Now(),
-		Filename:         filename,
-		BalanceAmountNum: sql.NullInt64{Int64: balanceAmount.Num().Int64(), Valid: true},
-		BalanceAmountDen: sql.NullInt64{Int64: balanceAmount.Denom().Int64(), Valid: true},
-		DateAsOf:         sql.NullTime{Time: dateAsOf, Valid: true},
-	})
-	if err != nil {
-		return nil, fmt.Errorf("save import: %w", err)
-	}
-	return &Import{imprt}, nil
-}
-
-type Import struct {
-	query.Import
-}
-
-func (s *Store) ListAccounts(ctx context.Context) ([]Account, error) {
-	var accounts []Account
-	ars, err := s.queries.ListAccounts(ctx)
-	if err != nil {
-		return accounts, fmt.Errorf("list accounts: %w", err)
-	}
-	for _, ar := range ars {
-		account := Account{
-			id:          ar.ID,
-			name:        ar.Name,
-			description: ar.Description.String,
-			accountType: AccountType{
-				ID:   ar.AccountTypeID,
-				Name: ar.AccountTypeName,
-			},
-		}
-		accounts = append(accounts, account)
-	}
-	return accounts, nil
-}
-
-func (s *Store) ListTransactions(ctx context.Context) ([]Transaction, error) {
-	var transactions []Transaction
-	txs, err := s.queries.ListTxs(ctx)
-	if err != nil {
-		return transactions, fmt.Errorf("gettxs: %w", err)
-	}
-	for _, tx := range txs {
-		transactions = append(transactions, Transaction{tx})
-	}
-	return transactions, nil
-}
-
-// SaveTransaction persists the transaction to storage.
-//
-// Duplicates can occur within the same import. Duplicates that occur
-// outside of a single will not be persisted.
-func (s *Store) SaveTransaction(ctx context.Context, datePosted time.Time, memo string, amount big.Rat, imprt Import) (*Transaction, error) {
-
-	duplicates, err := s.queries.GetDuplicateTx(ctx, query.GetDuplicateTxParams{DatePosted: datePosted, Memo: memo, AmountNum: amount.Num().Int64(), AmountDen: amount.Denom().Int64()})
-	if err != nil {
-		return nil, fmt.Errorf("check duplicates: %w", err)
-	}
-
-	duplicateExists := false
-	for _, duplicate := range duplicates {
-		if duplicate.ImportID != imprt.ID {
-			duplicateExists = true
-			break
-		}
-	}
-	if duplicateExists {
-		return nil, TransactionDuplicateError()
-	}
-
-	tx, err := s.queries.CreateTx(ctx, query.CreateTxParams{
-		DateCreated: time.Now(),
-		DatePosted:  datePosted,
-		Memo:        memo,
-		AmountNum:   amount.Num().Int64(),
-		AmountDen:   amount.Denom().Int64(),
-		ImportID:    imprt.ID,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("save transaction: %w", err)
-	}
-	return &Transaction{tx}, nil
-}
-
-// GetAmountFromNumDenom takes a numerator & denominator and prints a human readable dollar amount.
-func GetAmountFromNumDenom(n, d int64) string {
-	return strings.TrimRight(strings.TrimRight(big.NewRat(n, d).FloatString(100), "0"), ".")
 }
