63eba2aab4cd7ac73fa1eafbef42f8dbe7f93563
Rene Cunningham <rene@compounddata.com>
2023-06-18 10:20:04 +1000

    remove sqlc

--- db/queries/account_match.sql
+++
@@ -1,5 +0,0 @@
--- name: CreateAccountMatch :one
-INSERT INTO account_match (id, name, description, account_id) VALUES (NULL, ?, ?, ?) RETURNING *;
-
--- name: GetAccountMatch :one
-SELECT id, name, description, account_id FROM account_match WHERE name = ?;
--- db/queries/account_type.sql
+++
@@ -1,6 +0,0 @@
--- name: CreateAccountType :one
-INSERT INTO account_type (id, name) VALUES (NULL, ?) RETURNING *;
-
--- name: GetAccountType :one
-SELECT id, name FROM account_type WHERE name = ?;
-
